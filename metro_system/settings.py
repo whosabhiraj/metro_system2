f1715560234e9e6ad3908cc08d79807e93fd784e
Abhiraj <abhirajpharate@gmail.com>
2025-11-26 03:15:08 +0530

    Update settings.py

--- metro_system/settings.py
+++ metro_system/settings.py
@@ -183,6 +183,6 @@ SOCIALACCOUNT_PROVIDERS = {
 EMAIL_HOST_USER = 'metrosystem.otp@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_PASSWORD = 'ygza coxm tpuf qtwx'
+EMAIL_HOST_PASSWORD = '----------------'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
