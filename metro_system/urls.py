8fcaca4261bbf63bb12e9f4f420a028b2a726f22
Abhiraj <abhirajpharate@gmail.com>
2025-12-18 23:06:05 +0530

    final commit

--- metro_system/urls.py
+++ metro_system/urls.py
@@ -20,11 +20,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import settings
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ticket/', include('ticket.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', RedirectView.as_view(url='/ticket/', permanent=True)),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
